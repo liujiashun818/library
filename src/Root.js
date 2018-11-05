2a7ae5e395775ff2e01d75a65be6843382141d08
liujiashun818 <1696054400@qq.com>
2018-11-05 21:49:34 +0800

    xia

--- src/Root.js
+++ src/Root.js
@@ -1,17 +1,31 @@
 import $ from 'jquery';
 
 import React from 'react';
-import imgSrc from './includes/default.jpeg';
+import RainyDay from './commonfun/rainyday';
+import imgUrl from './includes/default.jpg';
 
 export default class Root extends React.Component {
   componentDidMount() {
-    $('#name').css('font-size', '40px');
+    function run() {
+      const image = document.getElementById('background');
+      image.onload = function () {
+        const engine = new RainyDay({
+          image: this,
+        });
+        engine.rain([[3, 2, 2]], 100);
+      };
+      image.crossOrigin = 'anonymous';
+      image.src = imgUrl;
+    }
+    run();
   }
   render() {
+    const width100 = {
+      width: '100%',
+      height: '100%'
+    }
     return (
-      <div style={{ textAlign: 'center' }}>
-        <h1 id="name">孔云ddd丽</h1>
-        <p id="imgStyle"><img src={imgSrc} alt="" /></p>
-      </div>);
+      <img id="background" alt="backgroundliujiashun" src="" style={width100} />
+    );
   }
 }
