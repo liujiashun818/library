46003f91cdef0d3dc32f5f70b785f10db7217bcc
liujiashun818 <1696054400@qq.com>
2018-11-05 20:53:49 +0800

    feat: my first

---
+++ src/app.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './Root';
+
+import './app.less';
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root'),
+);
---
+++ webpack.prod.js
@@ -0,0 +1,18 @@
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  entry: {
+    'app.bundle': './src/app.js',
+    contact: './src/contact.js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    inline: true,
+    port: 9000,
+    open: true,
+  },
+
+})
